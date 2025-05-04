19401907f0e9e42e55dc53c11b39d69f34be9fe9
Jeet <jeetnath2110@gmail.com>
2025-05-04 22:48:32 +0530

    Admin navigation logic done.

--- resources/views/components/admin-js.blade.php
+++ resources/views/components/admin-js.blade.php
@@ -1,5 +1,12 @@
 <script>
     $(document).ready(function() {
-        $(this).find('.admin-side-navigation ul li').eq(0).addClass('text-black bg-light');
+        $(this).find('.admin-side-navigation ul li').eq(0).addClass('text-black bg-light') && $(this).find('.admin-side-navigation ul li').css({
+            'cursor': 'pointer'
+        });
+
+        $(this).find('.admin-side-navigation ul').on('click', 'li', function() {
+            $(this).siblings().removeClass('text-black bg-light').addClass('text-light');
+            $(this).removeClass('text-light').addClass('text-black bg-light');
+        });
     });
 </script>
