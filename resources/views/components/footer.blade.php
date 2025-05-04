39e36b2cefd9b71ad24fd00a6f6abe80cf398870
Jeet <jeetnath2110@gmail.com>
2025-05-04 12:35:55 +0530

    Foter changes continue

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -20,11 +20,11 @@
                         <p class="text-uppercase fw-semibold text-light">about organisation</p>
                         <p class="text-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum magni autem incidunt, eum assumenda corporis!</p>
                         <div class="d-flex justify-content-start align-items-center mt-4">
-                            <a href="" class="border me-2 btn btn-outline-light shadow-none"><i class="bi bi-linkedin"></i></a>
-                            <a href="" class="border me-2 btn btn-outline-light shadow-none"><i class="bi bi-facebook"></i></a>
-                            <a href="" class="border me-2 btn btn-outline-light shadow-none"><i class="bi bi-twitter-x"></i></a>
-                            <a href="" class="border me-2 btn btn-outline-light shadow-none"><i class="bi bi-instagram"></i></a>
-                            <a href="" class="border btn btn-outline-light shadow-none"><i class="bi bi-threads"></i></a>
+                            <a href="" class="me-2 btn btn-outline-light shadow-none rounded-circle"><i class="bi bi-linkedin"></i></a>
+                            <a href="" class="me-2 btn btn-outline-light shadow-none rounded-circle"><i class="bi bi-facebook"></i></a>
+                            <a href="" class="me-2 btn btn-outline-light shadow-none rounded-circle"><i class="bi bi-twitter-x"></i></a>
+                            <a href="" class="me-2 btn btn-outline-light shadow-none rounded-circle"><i class="bi bi-instagram"></i></a>
+                            <a href="" class="btn btn-outline-light shadow-none rounded-circle"><i class="bi bi-threads"></i></a>
                         </div>
                     </div>
                 </div>
@@ -34,10 +34,18 @@
                     <div class="card-body">
                         <p class="text-uppercase fw-semibold text-light">quick links</p>
                         <ul class="ps-0">
-                            <a href="" class="text-decoration-none"><li class="list-style-none text-capitalize text-light mb-2">services</li></a>
-                            <a href="" class="text-decoration-none"><li class="list-style-none text-capitalize text-light mb-2">pricing</li></a>
-                            <a href="" class="text-decoration-none"><li class="list-style-none text-capitalize text-light mb-2">tech stack</li></a>
-                            <a href="" class="text-decoration-none"><li class="list-style-none text-capitalize text-light">portfolio</li></a>
+                            <a href="" class="text-decoration-none">
+                                <li class="list-style-none text-capitalize text-light mb-2">services</li>
+                            </a>
+                            <a href="" class="text-decoration-none">
+                                <li class="list-style-none text-capitalize text-light mb-2">pricing</li>
+                            </a>
+                            <a href="" class="text-decoration-none">
+                                <li class="list-style-none text-capitalize text-light mb-2">tech stack</li>
+                            </a>
+                            <a href="" class="text-decoration-none">
+                                <li class="list-style-none text-capitalize text-light">portfolio</li>
+                            </a>
                         </ul>
                     </div>
                 </div>
@@ -46,6 +54,23 @@
                 <div class="card border-0 bg-transparent d-flex justify-content-center align-items-center">
                     <div class="card-body">
                         <p class="text-uppercase fw-semibold text-light">locate us</p>
+                        <a href="mailto:jeetnath2110@gmail.com" target="_blank" class="text-light text-decoration-none">
+                            <div class="d-flex justify-content-start align-items-center mb-3">
+                                <div class="me-2">
+                                    <button type="button" class="btn btn-outline-light rounded-circle"><i class="bi bi-envelope-at-fill"></i></button>
+                                </div>
+                                <div>
+                                    <p class="mb-1">Mail us at :</p>
+                                    <p class="mb-0">jeetnath2110@gmail.com</p>
+                                </div>
+                            </div>
+                        </a>
+                        <a href="https://maps.app.goo.gl/CbdoXhYmqbe66RKt6" target="_blank" class="text-light text-decoration-none text-capitalize mt-3">
+                            <div class="d-flex align-items-center justify-content-start">
+                                <button type="button" class="btn btn-outline-light shadow-none rounded-circle me-2"><i class="bi bi-search"></i></button>
+                                <p class="mb-0">find us on google</p>
+                            </div>
+                        </a>
                     </div>
                 </div>
             </div>
