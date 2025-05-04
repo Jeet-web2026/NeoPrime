a42759aa46c8f1e1925d5a8fe5dd48569beb9dea
Jeet <jeetnath2110@gmail.com>
2025-05-04 21:40:21 +0530

    Admin pannel initial done.

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,13 +1,27 @@
-<x-SecondLayout subheading="Admin" bootstrap="active">
+<x-SecondLayout subheading="Admin" bootstrap="active" bIcons="active">
     <main id="admin-dashboard">
         <div class="row h-100">
             <div class="col-md-2">
-                <div class="card border-0 bg-transparent h-100 d-flex justify-content-center align-items-center">
+                <div class="card border-0 bg-transparent h-100">
                     <div class="card-body">
                         <a href="{{ route('admin-dashboard') }}" class="d-flex justify-content-center align-items-center">
                             <img src="{{ asset('essentials/images/website-logo-main.png') }}" alt="web-site-main-logo" class="admin-logo">
                         </a>
-                        <h5 class="text-light text-uppercase fw-semibold fs-5">admin panel</h5>
+                        <h5 class="text-light text-uppercase fw-semibold fs-5 text-center">super admin</h5>
+                        <div class="card border-0 bg-transparent d-flex justify-content-center align-items-center">
+                            <div class="card-body w-100 px-0">
+                                <ul class="ps-0 w-100">
+                                    <li class="text-capitalize fs-5 text-light bg-info bg-opacity-50 shadow-sm w-100 d-flex justify-content-start align-items-center p-2 rounded-3 mb-2">
+                                        <i class="bi bi-house-gear me-2"></i>
+                                        dashboard
+                                    </li>
+                                    <li class="text-capitalize fs-5 text-light shadow-sm w-100 d-flex justify-content-start align-items-center p-2 rounded-3">
+                                        <i class="bi bi-gear me-2"></i>
+                                        home
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
