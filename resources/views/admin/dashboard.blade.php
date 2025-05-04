6f4acb81e342efc1870564b0842a648ed201c669
Jeet <jeetnath2110@gmail.com>
2025-05-04 21:25:39 +0530

    Admin panel desigining continue

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,3 +1,22 @@
-<div>
-    <!-- Very little is needed to make a happy life. - Marcus Aurelius -->
-</div>
+<x-SecondLayout subheading="Admin" bootstrap="active">
+    <main id="admin-dashboard">
+        <div class="row h-100">
+            <div class="col-md-2">
+                <div class="card border-0 bg-transparent h-100 d-flex justify-content-center align-items-center">
+                    <div class="card-body">
+                        <a href="{{ route('admin-dashboard') }}" class="d-flex justify-content-center align-items-center">
+                            <img src="{{ asset('essentials/images/website-logo-main.png') }}" alt="web-site-main-logo" class="admin-logo">
+                        </a>
+                        <h5 class="text-light text-uppercase fw-semibold fs-5">admin panel</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-10 bg-primary bg-opacity-25">
+
+            </div>
+        </div>
+    </main>
+    @section('extracss')
+    <x-admin-css />
+    @endsection
+</x-SecondLayout>
---
+++ resources/views/components/admin-css.blade.php
@@ -0,0 +1,16 @@
+<style>
+    #admin-dashboard {
+        height: 100vh;
+        width: 100%;
+        background-color: #ffffff;
+    }
+
+    #admin-dashboard .col-md-2 {
+        background-color: #000046;
+    }
+
+    #admin-dashboard .col-md-2 .admin-logo {
+        height: 6rem;
+        width: 6rem;
+    }
+</style>
