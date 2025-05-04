8b816592c3f66712d426d87275b486111e8d9981
Jeet <jeetnath2110@gmail.com>
2025-05-04 14:12:02 +0530

    Popup running...

--- resources/views/components/home-popups.blade.php
+++ resources/views/components/home-popups.blade.php
@@ -1,21 +1,51 @@
 <div class="offcanvas offcanvas-start" tabindex="-1" id="hamburger-menu" aria-labelledby="hamburger-menuLabel">
-    <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="hamburger-menuLabel">Offcanvas</h5>
-        <button type="button" class="btn-close shadow-none" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    <div class="offcanvas-header" style="background-color: #2e008b;">
+        <h5 class="offcanvas-title fs-5 text-capitalize fw-semibold text-light" id="hamburger-menuLabel">service panel</h5>
+        <button type="button" class="btn-close shadow-none btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
-    <div class="offcanvas-body">
-        <div>
-            Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
-        </div>
-        <div class="dropdown mt-3">
-            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
-                Dropdown button
-            </button>
-            <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
+    <div class="offcanvas-body" style="background-color: #5404f51f;">
+        <div class="card border-0 bg-transparent">
+            <div class="card-body px-0">
+                <div class="card mb-3 border-0 shadow-sm position-relative py-1" style="background-color: #5404f5;">
+                    <div class="row g-0 align-items-center px-2">
+                        <div class="col-md-3">
+                            <button type="button" class="btn btn-outline-light shadow-none fs-4 w-100"><i class="bi bi-person-fill-gear"></i></button>
+                        </div>
+                        <div class="col-md-9">
+                            <div class="card-body">
+                                <h5 class="card-title text-uppercase fs-4 mb-0 fw-bold text-light text-center">admin login</h5>
+                                <a href="" class="stretched-link"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card mb-3 border-0 shadow-sm position-relative py-1" style="background-color: #5404f5;">
+                    <div class="row g-0 align-items-center px-2">
+                        <div class="col-md-3">
+                            <button type="button" class="btn btn-outline-light shadow-none fs-4 w-100"><i class="bi bi-person-fill-gear"></i></button>
+                        </div>
+                        <div class="col-md-9">
+                            <div class="card-body">
+                                <h5 class="card-title text-uppercase fs-4 mb-0 fw-bold text-light text-center">client's login</h5>
+                                <a href="" class="stretched-link"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card mb-3 border-0 shadow-sm position-relative py-1" style="background-color: #5404f5;">
+                    <div class="row g-0 align-items-center px-2">
+                        <div class="col-md-3">
+                            <button type="button" class="btn btn-outline-light shadow-none fs-4 w-100"><i class="bi bi-person-fill-gear"></i></button>
+                        </div>
+                        <div class="col-md-9">
+                            <div class="card-body">
+                                <h5 class="card-title text-uppercase fs-4 mb-0 fw-bold text-light text-center">partner's login</h5>
+                                <a href="" class="stretched-link"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
