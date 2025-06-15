7438340343673a6fc985bfd915391dbce170f4cd
Jeet <jeetnath2110@gmail.com>
2025-06-15 17:12:50 +0530

    Using Cashe for better experience

--- app/Http/Controllers/ServicesController.php
+++ app/Http/Controllers/ServicesController.php
@@ -3,18 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Models\OurServices;
+use Illuminate\Support\Facades\Cache;
 
 class ServicesController extends Controller
 {
     public function AllServices()
     {
-        $datas = OurServices::all();
+        $datas = Cache::remember('all_services', 60, function () {
+            return OurServices::all();
+        });
         return view('services.view-all-services', compact('datas'));
     }
 
     public function ViewServices($id)
     {
-        $data = OurServices::with('descriptionaditional')->findOrFail($id);
+        $cacheKey = "view_service_{$id}";
+
+        $data = Cache::remember($cacheKey, 60, function () use ($id) {
+            return OurServices::with('descriptionaditional')->findOrFail($id);
+        });
+
         return view('services.view-service', compact('data'));
     }
 }
