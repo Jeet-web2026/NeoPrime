5d30ac94204ed93ddfc3e740ce2805c282052dd3
Jeet <jeetnath2110@gmail.com>
2025-07-05 19:57:36 +0530

    client profile changing comtinue

--- resources/views/client/dashboard.blade.php
+++ resources/views/client/dashboard.blade.php
@@ -17,48 +17,77 @@
                     <button class="nav-link text-start text-black" id="v-pills-updates-tab" data-bs-toggle="pill" data-bs-target="#v-pills-updates" type="button" role="tab" aria-controls="v-pills-updates" aria-selected="false"><i class="bi bi-capslock me-3"></i>Updates</button>
                 </div>
             </div>
-            <div class="col-md-10 position-relative pt-5 px-2">
-                <nav class="navbar navbar-expand-lg shadow position-absolute top-0 start-0 w-100 bg-white">
+            <div class="col-md-10 position-relative pt-5 px-4">
+                <nav class="navbar navbar-expand-lg z-1 shadow position-absolute top-0 start-0 w-100 bg-white">
                     <div class="container-fluid">
-                        <a class="navbar-brand" href="#">Navbar</a>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li class="nav-item">
-                                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Link</a>
-                                </li>
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        Dropdown
-                                    </a>
-                                    <ul class="dropdown-menu">
-                                        <li><a class="dropdown-item" href="#">Action</a></li>
-                                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                                        <li>
-                                            <hr class="dropdown-divider">
-                                        </li>
-                                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            <div class="ms-auto d-flex align-items-center">
+                                <div class="dropdown me-2">
+                                    <button class="btn border-0 shadow-none" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        <div class="d-flex justify-content-center align-items-center">
+                                            <img src="{{ asset('essentials/images/client-default.jpg') }}" class="rounded-circle shadow-sm p-1 border" alt="{{ $clientName }}" style="height: 30px; width: 30px;">
+                                        </div>
+                                    </button>
+                                    <ul class="dropdown-menu mt-2 border-0 shadow-sm">
+                                        <li><a class="dropdown-item" href="#">Refer & Earn</a></li>
+                                        <li><a class="dropdown-item" href="#">Logout</a></li>
                                     </ul>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link disabled">Disabled</a>
-                                </li>
-                            </ul>
-                            <form class="d-flex" role="search">
-                                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                                <button class="btn btn-outline-success" type="submit">Search</button>
-                            </form>
+                                </div>
+                                <form class="d-flex" role="search">
+                                    <input class="form-control me-2 shadow-none" type="search" placeholder="Search" aria-label="Search">
+                                    <button class="btn btn-outline-success" type="submit">Search</button>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </nav>
                 <div class="tab-content" id="v-pills-tabContent">
-                    <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab" tabindex="0">...</div>
-                    <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab" tabindex="0">...</div>
+                    <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab" tabindex="0">
+
+                    </div>
+                    <div class="tab-pane fade mt-4" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab" tabindex="0">
+                        <div class="row">
+                            <div class="col-md-8 pe-1">
+                                <div class="card shadow border-0">
+                                    <div class="card-body">
+                                        <form>
+                                            <div class="mb-3">
+                                                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="exampleInputPassword1" class="form-label">Password</label>
+                                                <input type="password" class="form-control" id="exampleInputPassword1">
+                                            </div>
+                                            <div class="mb-3 form-check">
+                                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                                            </div>
+                                            <button type="submit" class="btn btn-primary">Submit</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4 ps-1">
+                                <div class="card shadow border-0">
+                                    <div class="card-body">
+                                        <div class="d-flex justify-content-center align-items-center mb-4">
+                                            <img src="{{ asset('essentials/images/client-default.jpg') }}" class="rounded-circle shadow-sm p-3 border" alt="{{ $clientName }}" style="height: 80px; width: 80px;">
+                                        </div>
+                                        <ul class="list-group">
+                                            <li class="list-group-item"><b>Name:</b> {{ $clientName }}</li>
+                                            <li class="list-group-item"><b>Address:</b> {{ $client->address . ', ' . $client->address_second . ', ' . $client->country . ', ' . $client->state . ', ' . $client->city . ', ' . $client->pincode }}</li>
+                                            <li class="list-group-item"><b>Email:</b> {{ $client->email }}</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <div class="tab-pane fade" id="v-pills-disabled" role="tabpanel" aria-labelledby="v-pills-disabled-tab" tabindex="0">...</div>
                     <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab" tabindex="0">...</div>
                     <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab" tabindex="0">...</div>
